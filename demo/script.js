d038d67c78d84ffd7fdafd745b69451564ee4fa4
s1nisteR <ankon1441@gmail.com>
2022-11-20 20:36:13 +0600

    Modified demo website to add event listeners to the proper buttons

--- demo/script.js
+++ demo/script.js
@@ -1,6 +1,8 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const login = document.getElementById('login');
+const register = document.getElementById('register');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -8,12 +10,18 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
+});
+
+login.addEventListener('click', () => {
 	fetch('http://127.0.0.1:8080', {
 		method: 'POST', // or 'PUT'
 		headers: {
 			'Content-Type': 'application/json',
-
 		},
 		body: 'SELECT * FROM customer',
-		});
+	});
+});
+
+register.addEventListener('click', () => {
+	//do queries for registration
 });
